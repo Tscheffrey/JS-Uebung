7ac28b63c531e651b70a651064a6f48dc6fffa7d
Hannes Schütz <hannes@fam-schuetz.de>
2018-04-23 11:06:35 +0200

    Add comments to Übung 3

--- Übung 3/main.js
+++ Übung 3/main.js
@@ -1,5 +1,12 @@
+// leeren String für die Ausgabe deklarieren
 var ergebnis = "";
-for (var i=0; i<10; i=i+1) {
+
+// die for-Schleife läuft von i=0 bis i=10
+for (var i=0; i < 10; i=i+1) {
+
+  // Ziffer aus i zum Ergebnis hinzufügen
   ergebnis = ergebnis + i;
+
+  // aktueller Wert der Variable ergebnis in der Konsole ausgeben
   console.log(ergebnis);
 }
