34f88deeacc7bfcbe232c90b977be953b3af7d85
Hannes Schütz <hannes@fam-schuetz.de>
2018-04-24 09:18:18 +0200

    add Übung 1a

---
+++ Tag 2/Übung 1 a/ajax-json.js
@@ -0,0 +1,21 @@
+function  fetchMessage()
+{
+  ajax  =  new  XMLHttpRequest();
+  ajax.open("GET","https://www.christoph-dietrich.de/daten-json.php", true);
+  ajax.onreadystatechange =  function(){
+    //  Kommunikation  beendet
+    if(this.readyState ==  4) {
+      //  HTTP-Rückmeldung  erfolgreich  (200)
+      if(this.status ==  200)  {
+        console.log(this);
+        //  Hier  vervollständigen,  um  erhaltende  Antwort  als  Text  in  „ausgabe“  auszugeben:
+        //var  antwortObjekt  =
+        //document.getElementById("ausgabe").innerText =
+      }
+      else  {
+        alert("Es  ist  ein  Fehler  aufgetreten!");
+      }
+    }
+  }
+  ajax.send();
+}
